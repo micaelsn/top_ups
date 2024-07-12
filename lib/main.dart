922aa11390ee776a557ab4c7fad1257b1b8e61b1
Micael Santos Nascimento <micaelnascimentocc@gmail.com>
2024-07-12 15:24:41 -0300

    fix: remove flag debug

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Top-ups',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
         useMaterial3: true,
       ),
       home: const HomePage(),
