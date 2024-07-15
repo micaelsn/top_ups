937e19022eb0cc75966752357450f5b120f433da
Micael Santos Nascimento <micaelnascimentocc@gmail.com>
2024-07-14 22:27:10 -0300

    add: functions on app controller

--- lib/features/app/presentation/controllers/app_controller.dart
+++ lib/features/app/presentation/controllers/app_controller.dart
@@ -1,8 +1,28 @@
+import 'package:flutter/material.dart';
+
 import '../../data/dtos/user_dto.dart';
+import '../../domain/entities/user_entity.dart';
 
 class AppController {
-  final _user = UserDTO(name: 'Micael', balance: 4000);
+  final ValueNotifier<UserEntity> _user =
+      ValueNotifier(UserDTO(name: 'Micael', balance: 4000));
 
-  String get userName => _user.name.isNotEmpty ? _user.name : 'User';
-  String get userBalance => _user.balance.toString();
+  ValueNotifier<UserEntity> get user => _user;
+  String get userName => user.value.name.isNotEmpty ? user.value.name : 'User';
+  bool get userVerified => user.value.token != null;
+
+  void rechargeMobile(double recharge) {
+    _transitionRate();
+    _recharge(recharge);
+  }
+
+  void _transitionRate() {
+    var newBalance = user.value.balance - 1.0;
+    user.value = user.value.copyWith(balance: newBalance);
+  }
+
+  void _recharge(double recharge) {
+    final newBalance = user.value.balance - recharge;
+    user.value = user.value.copyWith(balance: newBalance);
+  }
 }
